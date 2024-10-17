495eaa968c0372bbecda049777216d37fa785901
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-10-17 15:56:31 +0700

    feat: login component

--- elecTornHub_WPFBased/Components/LogPopup.xaml.cs
+++ elecTornHub_WPFBased/Components/LogPopup.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace elecTornHub_WPFBased.Components
 {
@@ -23,6 +11,36 @@ namespace elecTornHub_WPFBased.Components
         public LogPopup()
         {
             InitializeComponent();
+        }
+
+        // DependencyProperty for Type
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.Register("Type", typeof(string), typeof(LogPopup), new PropertyMetadata(string.Empty, OnTypeChanged));
+
+        public string Type // Changed from Text to Type
+        {
+            get { return (string)GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+
+        // Callback method for TypeProperty
+        private static void OnTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as LogPopup;
+            string newType = e.NewValue as string;
+
+            if (newType == "Login")
+            {
+                control.LogPopup_LoginText.Text = "Login";
+                control.LogPopup_LogButtonText.Content = "Login";
+                control.LogPopup_Redirect.Text = "Register?";
+            }
+            else if (newType == "Register")
+            {
+                control.LogPopup_LoginText.Text = "Register";
+                control.LogPopup_LogButtonText.Content = "Register";
+                control.LogPopup_Redirect.Text = "Login?";
+            }
         }
     }
 }
