a4724692e2e70e289c0ffbf5e53dd05b23b450bb
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-10-19 17:22:37 +0700

    feat: notif popup nonfunc

---
+++ elecTornHub_WPFBased/Components/ConfirmPopup.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+
+namespace elecTornHub_WPFBased.Components
+{
+    public partial class ConfirmPopup : UserControl, INotifyPropertyChanged
+    {
+        public ConfirmPopup()
+        {
+            InitializeComponent();
+            this.DataContext = this; // Set DataContext to itse
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // DependencyProperty for Title
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // DependencyProperty for Message_0
+        public static readonly DependencyProperty Message_0Property =
+            DependencyProperty.Register("Message_0", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_0
+        {
+            get { return (string)GetValue(Message_0Property); }
+            set { SetValue(Message_0Property, value); }
+        }
+
+        // DependencyProperty for Message_1
+        public static readonly DependencyProperty Message_1Property =
+            DependencyProperty.Register("Message_1", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_1
+        {
+            get { return (string)GetValue(Message_1Property); }
+            set { SetValue(Message_1Property, value); }
+        }
+
+        // DependencyProperty for Message_2
+        public static readonly DependencyProperty Message_2Property =
+            DependencyProperty.Register("Message_2", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_2
+        {
+            get { return (string)GetValue(Message_2Property); }
+            set { SetValue(Message_2Property, value); }
+        }
+
+        // DependencyProperty for Message_3
+        public static readonly DependencyProperty Message_3Property =
+            DependencyProperty.Register("Message_3", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_3
+        {
+            get { return (string)GetValue(Message_3Property); }
+            set { SetValue(Message_3Property, value); }
+        }
+
+        // DependencyProperty for Message_4
+        public static readonly DependencyProperty Message_4Property =
+            DependencyProperty.Register("Message_4", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_4
+        {
+            get { return (string)GetValue(Message_4Property); }
+            set { SetValue(Message_4Property, value); }
+        }
+
+        // DependencyProperty for Message_5
+        public static readonly DependencyProperty Message_5Property =
+            DependencyProperty.Register("Message_5", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_5
+        {
+            get { return (string)GetValue(Message_5Property); }
+            set { SetValue(Message_5Property, value); }
+        }
+
+        // DependencyProperty for Message_6
+        public static readonly DependencyProperty Message_6Property =
+            DependencyProperty.Register("Message_6", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_6
+        {
+            get { return (string)GetValue(Message_6Property); }
+            set { SetValue(Message_6Property, value); }
+        }
+
+        // DependencyProperty for Message_7
+        public static readonly DependencyProperty Message_7Property =
+            DependencyProperty.Register("Message_7", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty));
+        public string Message_7
+        {
+            get { return (string)GetValue(Message_7Property); }
+            set { SetValue(Message_7Property, value); }
+        }
+
+        // DependencyProperty for Type
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.Register("Type", typeof(string), typeof(ConfirmPopup), new PropertyMetadata(string.Empty, OnTypeChanged));
+
+        public string Type
+        {
+            get { return (string)GetValue(TypeProperty); }
+            set { SetValue(TypeProperty, value); }
+        }
+
+        // Callback method for TypeProperty
+        private static void OnTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ConfirmPopup control)
+            {
+                string newType = e.NewValue as string;
+
+                // Setup Default Values
+                control.ConfirmPopup_Title.Text = control.Title;
+                control.ConfirmPopup_OnYesNoButtonContainer.Visibility = Visibility.Collapsed;
+                control.ConfirmPopup_InputContainer.Visibility = Visibility.Collapsed;
+
+                switch (newType)
+                {
+                    case "Terbeli":
+                        // Additional logic for "Terbeli" can go here
+                        break;
+                    case "Kontak":
+                    case "Hapus Jualan":
+                    case "Hapus Post":
+                    case "Hapus Akun":
+                        control.ConfirmPopup_OnYesNoButtonContainer.Visibility = Visibility.Visible;
+                        control.ConfirmPopup_OnYesOnlyButton.Visibility = Visibility.Collapsed;
+                        break;
+                    case "Lapor":
+                    case "Takedown":
+                        control.ConfirmPopup_OnYesNoButtonContainer.Visibility = Visibility.Visible;
+                        control.ConfirmPopup_OnYesOnlyButton.Visibility = Visibility.Collapsed;
+                        control.ConfirmPopup_InputContainer.Visibility = Visibility.Visible;
+                        break;
+                    case "Akun Not Found":
+                        // Additional logic for "Akun Not Found" can go here
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ elecTornHub_WPFBased/Pages/CheckNotification.xaml.cs
@@ -0,0 +1,29 @@
+﻿using elecTornHub_WPFBased.Components;
+using elecTornHub_WPFBased.Extras;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace elecTornHub_WPFBased.Pages
+{
+    using static elecTornHub_WPFBased.Extras.Variables;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CheckNotification : Window
+    {
+        public CheckNotification()
+        {
+            InitializeComponent();
+            /*CheckNotification.PreviousWindow = this;
+            CheckNotification.LogPopup_LogButtonText.Click += (s, e) => CheckLogin();*/
+        }
+    }
+}
